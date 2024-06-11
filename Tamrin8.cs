1c7b6d15d5e12cb079556cd44bc51c9b05ed831e
امیرحسن اولادحسین <hassan79co@gmail.com>
2024-06-12 00:24:17 +0330

    Add files via upload

---
+++ Tamrin8.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int number=int.Parse(Console.ReadLine());
+            Console.WriteLine("the number 2 times the entred number equal to :" + tavan(number));
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
+        }
+        public static int tavan(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2 * tavan(number - 1);
+
+            }
+        }
+    }
+}
